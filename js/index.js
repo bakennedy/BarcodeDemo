e660e37cf64f019f42e4b33407c3005f8ef3946b
Brian Kennedy <brian.alex.kennedy@gmail.com>
2014-10-12 21:23:23 -0700

    Add list of scanned barcodes

--- js/index.js
+++ js/index.js
@@ -58,22 +58,17 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
-            /*
+
             if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                $("#codes").prepend("<tr><td>"+result.text+"</td></tr>")
+
             }
-            */
 
         }, function (error) { 
             console.log("Scanning failed: ", error); 
